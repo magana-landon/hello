0172a3be5c2cba36a591d882ceaa330d2d2e4f2f
magana-landon <lmagana3@huskers.unl.edu>
2022-08-25 13:33:19 +0000

    intitial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+ * Author: Landon Magana
+ * Date: 2022/08/25
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Landon Magana\n");
+
+  return 0;
+}
